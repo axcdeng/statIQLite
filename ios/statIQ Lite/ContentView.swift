8fca47b2dbbddf4bf62b44a46fb3c6ddf9b81222
c#sp <surxsc@gmail.com>
2026-02-15 15:10:45 -0800

    builded

---
+++ ios/statIQ Lite/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  statIQ Lite
+//
+//  Created by Alexander Deng on 2/14/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios/statIQ Lite/statIQ_LiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  statIQ_LiteApp.swift
+//  statIQ Lite
+//
+//  Created by Alexander Deng on 2/14/26.
+//
+
+import SwiftUI
+
+@main
+struct statIQ_LiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
